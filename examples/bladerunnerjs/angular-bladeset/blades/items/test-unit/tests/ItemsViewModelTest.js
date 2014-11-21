d602eac459aad1eb9701935dad02e5d970c89c0c
leggetter <phil@leggetter.co.uk>
2014-11-21 18:29:56 +0000

    Cleaning up unused tests. All remaining tests pass

--- examples/bladerunnerjs/angular-bladeset/blades/items/test-unit/tests/ItemsViewModelTest.js
+++
@@ -1,8 +0,0 @@
-var ItemsViewModelTest = TestCase( 'ItemsViewModelTest' );
-
-var ItemsViewModel = require( 'brjstodo/items/ItemsViewModel' );
-
-ItemsViewModelTest.prototype.testSomething = function() {
-  var model = new ItemsViewModel();
-  assertEquals( 'Welcome to your new Blade.', model.welcomeMessage() );
-};
